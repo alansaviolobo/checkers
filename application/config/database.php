5ffd6c6c2693f0f2a0dfea257f5d1cd2904b1945
agyzone <agyzone@07daca17-3fa5-4a6f-b644-4c2ff32e7089>
2009-04-22 06:03:03 +0000

    git-svn-id: https://checkers.unfuddle.com/svn/checkers_checkers@4 07daca17-3fa5-4a6f-b644-4c2ff32e7089

--- application/config/database.php
+++ application/config/database.php
@@ -38,9 +38,9 @@ $active_group = "default";
 $active_record = TRUE;
 
 $db['default']['hostname'] = "localhost";
-$db['default']['username'] = "";
-$db['default']['password'] = "";
-$db['default']['database'] = "";
+$db['default']['username'] = "checkers";
+$db['default']['password'] = "check123";
+$db['default']['database'] = "checkers";
 $db['default']['dbdriver'] = "mysql";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
