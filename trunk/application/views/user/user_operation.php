485f187ba75fe1514f58d5f1d79cfb41f1f7092b
agyzone <agyzone@07daca17-3fa5-4a6f-b644-4c2ff32e7089>
2009-04-24 04:29:31 +0000

    git-svn-id: https://checkers.unfuddle.com/svn/checkers_checkers@11 07daca17-3fa5-4a6f-b644-4c2ff32e7089

---
+++ trunk/application/views/user/user_operation.php
@@ -0,0 +1,51 @@
+<!--
+	Project Name: Checkers
+	Module Name: User Operations
+	Module Description: Provides Interface for all User related CRUD operations
+	Author: Agraj Agranayak
+	
+	Associated Files: controllers/user	
+-->
+
+<?php
+
+$attributes = array ('name'=>'frm_user_operation');
+echo form_open('user/new_user', $attributes);
+
+echo "<table cellpadding='2' style=\"text-align:left\">";
+
+echo "<tr><td>".form_label('Empployee Id', 'txt_emp_id')."</td>";
+echo "<td>".form_input('txt_emp_id')."</td><tr />";
+
+echo "<tr><td>".form_label('Name', 'txt_name')."</td>";
+echo "<td>".form_input('txt_name')."</td><tr />";
+
+echo "<tr><td>".form_label('Phone', 'txt_phone')."</td>";
+echo "<td>".form_input('txt_phone')."</td><tr />";
+
+echo "<tr><td>".form_label('Address', 'txt_address')."</td>";
+echo "<td>".form_input('txt_address')."</td></tr>";
+
+echo "<tr><td>".form_label('Joining Date', 'txt_joining_date')."</td>";
+echo "<td>".form_input('txt_joining_date', 'yyyy-mm-dd')."&nbsp";
+echo "<a href=\"javascript:showCal('cal_joining_date')\">select</a>"."</td><tr />";
+
+echo "<tr><td>".form_label('Designation', 'txt_designation')."</td>";
+echo "<td>".form_input('txt_designation')."</td><tr />";
+
+echo "<tr><td>".form_label('User Type', 'txt_user_type')."</td>";
+echo "<td>".form_input('txt_user_type')."</td><tr />";
+
+echo "<tr><td>".form_label('User Privileges', 'txt_user_privileges')."</td>";
+echo "<td>".form_checkbox('chk_user_manage','User Manage')."<br />";
+echo form_checkbox('chk_menu_manage','Menu Manage')."<br />";
+echo form_checkbox('chk_database_manage','Database Manage')."<br />";
+echo form_checkbox('chk_report_manage','Report Manage')."<br />";
+echo form_checkbox('chk_billing','Billing')."</td><tr />";
+
+echo "<tr><td>".form_submit('submit', 'Insert')."&nbsp;";
+echo form_reset('reset', 'Clear')."</td></tr>";
+
+echo "</table>";
+echo form_close();
+?>
