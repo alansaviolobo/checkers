6e00279b856c186cfe0cc7e3d17d2c20fc23bc23
agyzone <agyzone@07daca17-3fa5-4a6f-b644-4c2ff32e7089>
2009-04-25 22:49:02 +0000

    git-svn-id: https://checkers.unfuddle.com/svn/checkers_checkers@18 07daca17-3fa5-4a6f-b644-4c2ff32e7089

--- trunk/prototype/homepage_files/axurerp_pagespecificscript.js
+++ trunk/prototype/homepage_files/axurerp_pagespecificscript.js
@@ -42,108 +42,8 @@ function OnLoad(e) {
 
 }
 
-var u20 = document.getElementById('u20');
-
-var u64 = document.getElementById('u64');
-
-var u51 = document.getElementById('u51');
-
-var u36 = document.getElementById('u36');
-
-var u31 = document.getElementById('u31');
-gv_vAlignTable['u31'] = 'center';
-var u45 = document.getElementById('u45');
-gv_vAlignTable['u45'] = 'center';
-var u11 = document.getElementById('u11');
-
-u11.style.cursor = 'pointer';
-if (bIE) u11.attachEvent("onclick", Clicku11);
-else u11.addEventListener("click", Clicku11, true);
-function Clicku11(e)
-{
-
-if (true) {
-
-	self.location.href="report.html" + GetQuerystring();
-
-}
-
-}
-
-var u27 = document.getElementById('u27');
-
-var u6 = document.getElementById('u6');
-gv_vAlignTable['u6'] = 'top';
-var u4 = document.getElementById('u4');
-gv_vAlignTable['u4'] = 'top';
-var u2 = document.getElementById('u2');
-
-var u10 = document.getElementById('u10');
-gv_vAlignTable['u10'] = 'top';
-var u0 = document.getElementById('u0');
-gv_vAlignTable['u0'] = 'top';
-var u26 = document.getElementById('u26');
-gv_vAlignTable['u26'] = 'center';
-var u49 = document.getElementById('u49');
-
-var u63 = document.getElementById('u63');
-
-var u35 = document.getElementById('u35');
-
-var u29 = document.getElementById('u29');
-
-var u54 = document.getElementById('u54');
-
-var u8 = document.getElementById('u8');
-gv_vAlignTable['u8'] = 'top';
-var u34 = document.getElementById('u34');
-gv_vAlignTable['u34'] = 'center';
-var u14 = document.getElementById('u14');
-gv_vAlignTable['u14'] = 'top';
-var u48 = document.getElementById('u48');
-gv_vAlignTable['u48'] = 'center';
-var u28 = document.getElementById('u28');
-gv_vAlignTable['u28'] = 'center';
-var u44 = document.getElementById('u44');
-
-var u33 = document.getElementById('u33');
-
-var u50 = document.getElementById('u50');
-gv_vAlignTable['u50'] = 'center';
-var u22 = document.getElementById('u22');
-gv_vAlignTable['u22'] = 'top';
-var u52 = document.getElementById('u52');
-
-var u13 = document.getElementById('u13');
-
-u13.style.cursor = 'pointer';
-if (bIE) u13.attachEvent("onclick", Clicku13);
-else u13.addEventListener("click", Clicku13, true);
-function Clicku13(e)
-{
-
-if (true) {
-
-	self.location.href="login.html" + GetQuerystring();
-
-}
-
-}
-
-var u47 = document.getElementById('u47');
-
-var u12 = document.getElementById('u12');
-gv_vAlignTable['u12'] = 'top';
-var u41 = document.getElementById('u41');
-
-var u53 = document.getElementById('u53');
-gv_vAlignTable['u53'] = 'center';
-var u57 = document.getElementById('u57');
-
-var u21 = document.getElementById('u21');
-gv_vAlignTable['u21'] = 'center';
-var u37 = document.getElementById('u37');
-gv_vAlignTable['u37'] = 'center';
+var u16 = document.getElementById('u16');
+gv_vAlignTable['u16'] = 'top';
 var u7 = document.getElementById('u7');
 
 u7.style.cursor = 'pointer';
@@ -160,10 +60,32 @@ if (true) {
 
 }
 
-var u40 = document.getElementById('u40');
+var u15 = document.getElementById('u15');
+gv_vAlignTable['u15'] = 'top';
+var u2 = document.getElementById('u2');
 
-var u17 = document.getElementById('u17');
+var u19 = document.getElementById('u19');
+gv_vAlignTable['u19'] = 'center';
+var u13 = document.getElementById('u13');
 
+u13.style.cursor = 'pointer';
+if (bIE) u13.attachEvent("onclick", Clicku13);
+else u13.addEventListener("click", Clicku13, true);
+function Clicku13(e)
+{
+
+if (true) {
+
+	self.location.href="login.html" + GetQuerystring();
+
+}
+
+}
+
+var u22 = document.getElementById('u22');
+gv_vAlignTable['u22'] = 'top';
+var u12 = document.getElementById('u12');
+gv_vAlignTable['u12'] = 'top';
 var u5 = document.getElementById('u5');
 
 u5.style.cursor = 'pointer';
@@ -180,10 +102,16 @@ if (true) {
 
 }
 
-var u15 = document.getElementById('u15');
-gv_vAlignTable['u15'] = 'top';
-var u56 = document.getElementById('u56');
-gv_vAlignTable['u56'] = 'center';
+var u8 = document.getElementById('u8');
+gv_vAlignTable['u8'] = 'top';
+var u10 = document.getElementById('u10');
+gv_vAlignTable['u10'] = 'top';
+var u0 = document.getElementById('u0');
+gv_vAlignTable['u0'] = 'top';
+var u21 = document.getElementById('u21');
+gv_vAlignTable['u21'] = 'center';
+var u17 = document.getElementById('u17');
+
 var u3 = document.getElementById('u3');
 
 u3.style.cursor = 'pointer';
@@ -200,22 +128,10 @@ if (true) {
 
 }
 
-var u65 = document.getElementById('u65');
-gv_vAlignTable['u65'] = 'center';
-var u1 = document.getElementById('u1');
-
-var u25 = document.getElementById('u25');
-
-var u59 = document.getElementById('u59');
-gv_vAlignTable['u59'] = 'center';
-var u43 = document.getElementById('u43');
-
-var u16 = document.getElementById('u16');
-gv_vAlignTable['u16'] = 'top';
-var u39 = document.getElementById('u39');
-gv_vAlignTable['u39'] = 'center';
-var u19 = document.getElementById('u19');
-gv_vAlignTable['u19'] = 'center';
+var u14 = document.getElementById('u14');
+gv_vAlignTable['u14'] = 'top';
+var u6 = document.getElementById('u6');
+gv_vAlignTable['u6'] = 'top';
 var u9 = document.getElementById('u9');
 
 u9.style.cursor = 'pointer';
@@ -232,30 +148,28 @@ if (true) {
 
 }
 
-var u30 = document.getElementById('u30');
+var u20 = document.getElementById('u20');
 
-var u60 = document.getElementById('u60');
+var u1 = document.getElementById('u1');
 
-var u24 = document.getElementById('u24');
+var u11 = document.getElementById('u11');
 
-var u46 = document.getElementById('u46');
+u11.style.cursor = 'pointer';
+if (bIE) u11.attachEvent("onclick", Clicku11);
+else u11.addEventListener("click", Clicku11, true);
+function Clicku11(e)
+{
 
-var u55 = document.getElementById('u55');
+if (true) {
 
-var u38 = document.getElementById('u38');
+	self.location.href="report.html" + GetQuerystring();
 
-var u61 = document.getElementById('u61');
+}
+
+}
 
 var u18 = document.getElementById('u18');
 
-var u62 = document.getElementById('u62');
-gv_vAlignTable['u62'] = 'center';
-var u32 = document.getElementById('u32');
-
-var u42 = document.getElementById('u42');
-gv_vAlignTable['u42'] = 'center';
-var u23 = document.getElementById('u23');
-
-var u58 = document.getElementById('u58');
-
+var u4 = document.getElementById('u4');
+gv_vAlignTable['u4'] = 'top';
 if (window.OnLoad) OnLoad();
