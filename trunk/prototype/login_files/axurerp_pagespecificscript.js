4cb2224e723e982eda097e29a57bfed5185fd816
agyzone <agyzone@07daca17-3fa5-4a6f-b644-4c2ff32e7089>
2009-04-25 22:08:59 +0000

    git-svn-id: https://checkers.unfuddle.com/svn/checkers_checkers@16 07daca17-3fa5-4a6f-b644-4c2ff32e7089

---
+++ trunk/prototype/login_files/axurerp_pagespecificscript.js
@@ -0,0 +1,83 @@
+
+var PageName = 'login';
+var PageId = 'pcb5a18eb17a14040a46e0e596938e975'
+var PageUrl = 'login.html'
+document.title = 'login';
+
+if (top.location != self.location)
+{
+	if (parent.HandleMainFrameChanged) {
+		parent.HandleMainFrameChanged();
+	}
+}
+
+var $OnLoadVariable = '';
+
+var $CSUM;
+
+var hasQuery = false;
+var query = window.location.hash.substring(1);
+if (query.length > 0) hasQuery = true;
+var vars = query.split("&");
+for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0].length > 0) eval("$" + pair[0] + " = decodeURIComponent(pair[1]);");
+} 
+
+if (hasQuery && $CSUM != 1) {
+alert('Prototype Warning: Variable values were truncated.');
+}
+
+function GetQuerystring() {
+    return '#OnLoadVariable=' + encodeURIComponent($OnLoadVariable) + '&CSUM=1';
+}
+
+function PopulateVariables(value) {
+  value = value.replace(/\[\[OnLoadVariable\]\]/g, $OnLoadVariable);
+  value = value.replace(/\[\[PageName\]\]/g, PageName);
+  return value;
+}
+
+function OnLoad(e) {
+
+}
+
+var u10 = document.getElementById('u10');
+gv_vAlignTable['u10'] = 'center';
+var u5 = document.getElementById('u5');
+gv_vAlignTable['u5'] = 'top';
+var u0 = document.getElementById('u0');
+
+var u3 = document.getElementById('u3');
+gv_vAlignTable['u3'] = 'top';
+var u9 = document.getElementById('u9');
+
+var u6 = document.getElementById('u6');
+gv_vAlignTable['u6'] = 'top';
+var u1 = document.getElementById('u1');
+
+var u4 = document.getElementById('u4');
+
+u4.style.cursor = 'pointer';
+if (bIE) u4.attachEvent("onclick", Clicku4);
+else u4.addEventListener("click", Clicku4, true);
+function Clicku4(e)
+{
+
+if (true) {
+
+	self.location.href="homepage.html" + GetQuerystring();
+
+}
+
+}
+
+var u11 = document.getElementById('u11');
+gv_vAlignTable['u11'] = 'top';
+var u7 = document.getElementById('u7');
+
+var u2 = document.getElementById('u2');
+gv_vAlignTable['u2'] = 'top';
+var u8 = document.getElementById('u8');
+gv_vAlignTable['u8'] = 'center';
+if (window.OnLoad) OnLoad();
