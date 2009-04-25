53697750aee32c47a69791a99de9776697507d0a
agyzone <agyzone@07daca17-3fa5-4a6f-b644-4c2ff32e7089>
2009-04-25 23:05:16 +0000

    git-svn-id: https://checkers.unfuddle.com/svn/checkers_checkers@21 07daca17-3fa5-4a6f-b644-4c2ff32e7089

---
+++ trunk/prototype/database_backup_files/axurerp_pagespecificscript.js
@@ -0,0 +1,75 @@
+
+var PageName = 'database backup';
+var PageId = 'p8bca590f8c4d4067831375f304f693a8'
+var PageUrl = 'database_backup.html'
+document.title = 'database backup';
+
+if (top.location != self.location)
+{
+	if (parent.HandleMainFrameChanged) {
+		parent.HandleMainFrameChanged();
+	}
+}
+
+var $OnLoadVariable = '';
+
+var $CSUM;
+
+var hasQuery = false;
+var query = window.location.hash.substring(1);
+if (query.length > 0) hasQuery = true;
+var vars = query.split("&");
+for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0].length > 0) eval("$" + pair[0] + " = decodeURIComponent(pair[1]);");
+} 
+
+if (hasQuery && $CSUM != 1) {
+alert('Prototype Warning: Variable values were truncated.');
+}
+
+function GetQuerystring() {
+    return '#OnLoadVariable=' + encodeURIComponent($OnLoadVariable) + '&CSUM=1';
+}
+
+function PopulateVariables(value) {
+  value = value.replace(/\[\[OnLoadVariable\]\]/g, $OnLoadVariable);
+  value = value.replace(/\[\[PageName\]\]/g, PageName);
+  return value;
+}
+
+function OnLoad(e) {
+
+}
+
+var u5 = document.getElementById('u5');
+
+var u0 = document.getElementById('u0');
+gv_vAlignTable['u0'] = 'top';
+var u3 = document.getElementById('u3');
+
+var u6 = document.getElementById('u6');
+gv_vAlignTable['u6'] = 'center';
+var u1 = document.getElementById('u1');
+
+u1.style.cursor = 'pointer';
+if (bIE) u1.attachEvent("onclick", Clicku1);
+else u1.addEventListener("click", Clicku1, true);
+function Clicku1(e)
+{
+
+if (true) {
+
+	self.location.href="database.html" + GetQuerystring();
+
+}
+
+}
+
+var u4 = document.getElementById('u4');
+gv_vAlignTable['u4'] = 'center';
+var u7 = document.getElementById('u7');
+gv_vAlignTable['u7'] = 'top';
+var u2 = document.getElementById('u2');
+gv_vAlignTable['u2'] = 'top';
+if (window.OnLoad) OnLoad();
