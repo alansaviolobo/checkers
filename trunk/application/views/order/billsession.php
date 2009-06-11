587a68ecc67acdb825b96ecd8f10b0e693e0f70d
agyzone <agyzone@07daca17-3fa5-4a6f-b644-4c2ff32e7089>
2009-06-11 16:04:06 +0000

    Deleted unnecessary files from the view folder.
    git-svn-id: https://checkers.unfuddle.com/svn/checkers_checkers@38 07daca17-3fa5-4a6f-b644-4c2ff32e7089

--- trunk/application/views/order/billsession.php
+++
@@ -1,7 +0,0 @@
-<?php
-if(isset($_POST['discount']) && isset($_POST['tax']))
-{
-	$this->session->set_userdata('discount',$this->input->xss_clean($this->input->post('discount')));
-	$this->session->set_userdata('tax',$this->input->xss_clean($this->input->post('tax')));
-}
-?>
