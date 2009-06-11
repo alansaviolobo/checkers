c70d0923354e124b4bbd7480050036ac47efe511
agyzone <agyzone@07daca17-3fa5-4a6f-b644-4c2ff32e7089>
2009-06-11 16:40:47 +0000

    Bug: 6 Cost is fixed
    git-svn-id: https://checkers.unfuddle.com/svn/checkers_checkers@42 07daca17-3fa5-4a6f-b644-4c2ff32e7089

--- trunk/application/controllers/menu_c.php
+++ trunk/application/controllers/menu_c.php
@@ -8,96 +8,110 @@ class Menu_c extends Controller
 
     function index()
     {
-    	$this->menu_list_source('Food','');
+        $this->menu_list_source('Food', '');
     }
 
     function main()
     {
-		if ( isset ($_POST['btn_source_menu']))
-		{
-			$source_type = $this->input->xss_clean($this->input->post('ddl_source_type'));
-			$this->menu_list_source($source_type,'');
-		}
+        if ( isset ($_POST['btn_source_menu']))
+        {
+            $source_type = $this->input->xss_clean($this->input->post('ddl_source_type'));
+            $this->menu_list_source($source_type, '');
+        }
     }
 
-    function menu_list_source($source_type,$message)
+    function menu_list_source($source_type, $message)
     {
-    	$this->load->model('menu_m', '', TRUE);
-		
-    	$config['base_url'] = base_url().'index.php/menu_c/index';
+        $this->load->model('menu_m', '', TRUE);
+
+        $config['base_url'] = base_url().'index.php/menu_c/index';
         $config['total_rows'] = $this->menu_m->menu_list_source_count($source_type);
-		$config['full_tag_open'] = '<p>';
-		$config['full_tag_close'] = '</p>';
+        $config['full_tag_open'] = '<p>';
+        $config['full_tag_close'] = '</p>';
         $config['per_page'] = 15;
         $config['uri_segment'] = 3;
 
         $this->pagination->initialize($config);
-		
-        $data['menu_items'] = $this->menu_m->menu_list_source($source_type,$config['per_page'],$this->uri->segment(3));
-		$data['source_type'] = $source_type;
-		$data['results'] = $config['total_rows'];
-		$data['ticket'] = $this->ticket_m->ticket_list();
-    	$content['title'] = "Menu List";
-		$content['section_menu'] = 'misc/top_menu';
+
+        $data['menu_items'] = $this->menu_m->menu_list_source($source_type, $config['per_page'], $this->uri->segment(3));
+        $data['source_type'] = $source_type;
+        $data['results'] = $config['total_rows'];
+        $data['ticket'] = $this->ticket_m->ticket_list();
+        $content['title'] = "Menu List";
+        $content['section_menu'] = 'misc/top_menu';
         $content['section_content'] = 'menu/menu_list';
-		$content['message'] = $message;
+        $content['message'] = $message;
         $this->load->vars($content);
         $this->load->view('template/template_all', $data);
     }
-	
-	function menu_add()
-	{
-		$source_type = $this->input->xss_clean($this->input->post('ddl_source_type'));
-		$name = $this->input->xss_clean($this->input->post('txt_name'));
-		$cost = $this->input->xss_clean($this->input->post('txt_cost'));
-		$check = $this->menu_m->menu_add($source_type,$name,$cost);
-		$this->menu_list_source($source_type, $check);
-	}
-	
-	function menu_manage($name)
-	{
-		$data['menu'] = $this->menu_m->menu_list_menu($name);
-		$data['ticket'] = $this->ticket_m->ticket_list();
-		$content['title'] = "Menu Manage";
-		$content['section_menu'] = 'misc/top_menu';
+
+    function menu_add()
+    {
+        $source_type = $this->input->xss_clean($this->input->post('ddl_source_type'));
+        $name = $this->input->xss_clean($this->input->post('txt_name'));
+        $cost = $this->input->xss_clean($this->input->post('txt_cost'));
+        if ($cost == null)
+        {
+            $check = 'Please Fill The Cost.';
+        }
+        else
+        {
+            $check = $this->menu_m->menu_add($source_type, $name, $cost);
+        }
+        $this->menu_list_source($source_type, $check);
+    }
+
+    function menu_manage($name)
+    {
+        $data['menu'] = $this->menu_m->menu_list_menu($name);
+        $data['ticket'] = $this->ticket_m->ticket_list();
+        $content['title'] = "Menu Manage";
+        $content['section_menu'] = 'misc/top_menu';
         $content['section_content'] = 'menu/menu_manage';
         $this->load->vars($content);
-        $this->load->view('template/template_all',$data);
-	}
-	
-	function menu_update()
-	{
-		$name = $this->input->xss_clean($this->input->post('name'));
-		$source_type = $this->input->xss_clean($this->input->post('ddl_source_type'));
-		$new_name = $this->input->xss_clean($this->input->post('txt_name'));
-		$cost = $this->input->xss_clean($this->input->post('txt_cost'));
-		$check = $this->menu_m->menu_update($name,$source_type,$new_name,$cost);
-		$this->menu_list_source($source_type, $check);
-	}
-	
-	function menu_delete($name,$source_type)
-	{
-		$check = $this->menu_m->menu_delete($name);
-		$this->menu_list_source($source_type, $check);
-	}
-	
-	function menu_purchase()
-	{
-		$content['title'] = "Menu Purchase";
-		$content['section_menu'] = 'misc/top_menu';
+        $this->load->view('template/template_all', $data);
+    }
+
+    function menu_update()
+    {
+        $name = $this->input->xss_clean($this->input->post('name'));
+        $source_type = $this->input->xss_clean($this->input->post('ddl_source_type'));
+        $new_name = $this->input->xss_clean($this->input->post('txt_name'));
+        $cost = $this->input->xss_clean($this->input->post('txt_cost'));
+        if ($cost == null)
+        {
+            $check = 'Please Fill The Cost.';
+        }
+        else
+        {
+            $check = $this->menu_m->menu_update($name, $source_type, $new_name, $cost);
+        }
+        $this->menu_list_source($source_type, $check);
+    }
+
+    function menu_delete($name, $source_type)
+    {
+        $check = $this->menu_m->menu_delete($name);
+        $this->menu_list_source($source_type, $check);
+    }
+
+    function menu_purchase()
+    {
+        $content['title'] = "Menu Purchase";
+        $content['section_menu'] = 'misc/top_menu';
         $content['section_content'] = 'menu/menu_purchase';
-		$data['menu'] = $this->menu_m->menu_list_source('Bar','','');
-		$data['ticket'] = $this->ticket_m->ticket_list();
+        $data['menu'] = $this->menu_m->menu_list_source('Bar', '', '');
+        $data['ticket'] = $this->ticket_m->ticket_list();
         $this->load->vars($content);
-        $this->load->view('template/template_all',$data);
-	}
-	
-	function menu_purchase_insert()
-	{
-		$menu = $this->input->xss_clean($this->input->post('ddl_source_bar'));
-		$quantity = $this->input->xss_clean($this->input->post('txt_quantity'));
-		$check = $this->ticket_m->menu_purchase_insert($menu,$quantity);
-		$this->menu_list_source('Bar', $check);
-	}
+        $this->load->view('template/template_all', $data);
+    }
+
+    function menu_purchase_insert()
+    {
+        $menu = $this->input->xss_clean($this->input->post('ddl_source_bar'));
+        $quantity = $this->input->xss_clean($this->input->post('txt_quantity'));
+        $check = $this->ticket_m->menu_purchase_insert($menu, $quantity);
+        $this->menu_list_source('Bar', $check);
+    }
 }
 ?>
