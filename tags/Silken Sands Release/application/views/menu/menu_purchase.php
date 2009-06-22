28e384c9542bf44f5b05936bf4e570ca25e3f39d
alansaviolobo <alansaviolobo@07daca17-3fa5-4a6f-b644-4c2ff32e7089>
2009-06-22 06:30:09 +0000

    fixed #42 added js validation for purchases form
    git-svn-id: https://checkers.unfuddle.com/svn/checkers_checkers@95 07daca17-3fa5-4a6f-b644-4c2ff32e7089

--- tags/Silken Sands Release/application/views/menu/menu_purchase.php
+++ tags/Silken Sands Release/application/views/menu/menu_purchase.php
@@ -1,4 +1,11 @@
 <?php
+echo "<script language='javascript'>";
+echo "function checkform(form) {";
+echo "if (form.ddl_source_bar.value == '') { alert('Please select Category'); return false; };";
+echo "if (form.txt_quantity.value == '') { alert('Please enter a quantity'); return false; };";
+echo "return true;";
+echo "}";
+echo "</script>";
 $bar_items = array ('' => 'Select One');
 foreach ($menu as $m) $bar_items[$m['name']] = $m['name'];
 
@@ -12,10 +19,10 @@ echo "$jsarray\n";
 echo "document.getElementById('current').value = ticket[item]|0;";
 echo "}";
 echo "</script>";
-echo form_open('menu_c/menu_purchase_insert', array ('name'=>'frm_menu_purchase'));
+echo form_open('menu_c/menu_purchase_insert', array ('name'=>'frm_menu_purchase', 'onsubmit'=>'return checkform(this)'));
 echo "<h2>Purchase Menu Items</h2>";
 echo "<table cellpadding='3' cellspacing='3'>";
-echo "<tr><td>".form_label('Item', 'ddl_source_type')."</td>";
+echo "<tr><td>".form_label('Item', 'ddl_source_bar')."</td>";
 echo "<td>".form_dropdown('ddl_source_bar', $bar_items, '', "onchange='changeqty(this.value)'")."</td></tr>";
 echo "<tr><td>".form_label('Current Quantity', 'current')."</td>";
 echo "<td>".form_input('current', '', "id='current' size='5' readonly")."</td></tr>";
