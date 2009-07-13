7c5b6b85760134d07fd5a02937a8b33948f61a85
alansaviolobo <alansaviolobo@07daca17-3fa5-4a6f-b644-4c2ff32e7089>
2009-07-13 18:37:04 +0000

    BugId: 00 new db structure supports support for billing & inventory with activity logging
    git-svn-id: https://checkers.unfuddle.com/svn/checkers_checkers@103 07daca17-3fa5-4a6f-b644-4c2ff32e7089

--- trunk/setup/Install.sql
+++ trunk/setup/Install.sql
@@ -1,8 +1,6 @@
-﻿-- MySQL Administrator dump 1.4
---
 -- ------------------------------------------------------
--- Server version	5.1.30-community
-
+-- Checkers Complete Database Structure
+-- ------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -22,86 +20,175 @@ CREATE DATABASE IF NOT EXISTS checkers;
 USE checkers;
 
 --
--- Definition of table `bill`
+-- Definition of table `users`
 --
 
-DROP TABLE IF EXISTS `bill`;
-CREATE TABLE `bill` (
-  `number` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `dated` date DEFAULT NULL,
-  `beverages` int(10) unsigned DEFAULT NULL,
-  `bar` int(10) unsigned NOT NULL DEFAULT 0,
-  `food` int(10) unsigned NOT NULL DEFAULT 0,
-  `subtotal` int(10) unsigned NOT NULL DEFAULT 0,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` char(40) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `designation` ENUM('waiter', 'manager', 'owner', 'chef') NOT NULL,
+  `enabled` BOOL NOT NULL default true,
+  `access_users` ENUM('none', 'view', 'edit') NOT NULL DEFAULT 'none',
+  `access_menus` ENUM('none', 'view', 'edit') NOT NULL DEFAULT 'none',
+  `access_orders` ENUM('none', 'view', 'edit') NOT NULL DEFAULT 'none',
+  `access_bills` ENUM('none', 'view', 'edit') NOT NULL DEFAULT 'none',
+  `access_reports` ENUM('none', 'view', 'edit') NOT NULL DEFAULT 'none',
+  `access_database` ENUM('none', 'view', 'edit') NOT NULL DEFAULT 'none',
+  `access_raw_materials` ENUM('none', 'view', 'edit') NOT NULL DEFAULT 'none',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`username`)
+) ENGINE=InnoDB;
+
+--
+-- Definition of table `raw_materials`
+--
+
+DROP TABLE IF EXISTS `raw_materials`;
+CREATE TABLE `raw_materials` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `price` int(10) unsigned NOT NULL,
+  `warning_qty` int(10) unsigned NOT NULL,
+  `enabled` BOOL NOT NULL default true,
+  PRIMARY KEY (`id`),
+) ENGINE=InnoDB;
+
+--
+-- Definition of table `raw_material_purchases`
+--
+
+DROP TABLE IF EXISTS `raw_material_purchases`;
+CREATE TABLE `raw_material_purchases` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `raw_material_id` int(10) unsigned NOT NULL,
+  `quantity` int(10) unsigned NOT NULL,
+  `bought_on` date NOT NULL,
+  `entered_by` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`raw_material_id`) REFERENCES `raw_materials`(`id`)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`entered_by`) REFERENCES `users`(`id`)
+  ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+--
+-- Definition of table `raw_material_menu_map`
+--
+
+DROP TABLE IF EXISTS `raw_material_menu_map`;
+CREATE TABLE `raw_material_menu_map` (
+  `menu_item_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `raw_material_id` int(10) unsigned NOT NULL,
+  `usage_fraction` float unsigned NOT NULL,
+  FOREIGN KEY (`raw_material_id`) REFERENCES `raw_materials`(`id`)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`menu_item_id`) REFERENCES `menu_items`(`id`)
+  ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+--
+-- Definition of table `locations`
+--
+
+DROP TABLE IF EXISTS `locations`;
+CREATE TABLE `locations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`name`)
+) ENGINE=InnoDB;
+
+--
+-- Definition of table `menu_sections`
+--
+
+DROP TABLE IF EXISTS `menu_sections`;
+CREATE TABLE `menu_sections` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `enabled` BOOL NOT NULL default true,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`name`)
+) ENGINE=InnoDB;
+
+--
+-- Definition of table `menu_items`
+--
+
+DROP TABLE IF EXISTS `menu_items`;
+CREATE TABLE `menu_items` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `price` int(10) unsigned NOT NULL,
+  `menu_section_id` int(10) unsigned NOT NULL,
+  `enabled` BOOL NOT NULL default true,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY (`name`),
+  FOREIGN KEY (`menu_section_id`) REFERENCES `menu_sections`(`id`)
+  ON DELETE RESTRICT ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+--
+-- Definition of table `bills`
+--
+
+DROP TABLE IF EXISTS `bills`;
+CREATE TABLE `bills` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `customer_name` varchar(255) DEFAULT NULL,
+  `waiter_id` int(10) unsigned NOT NULL,
+  `location_id` int(10) unsigned NOT NULL,
+  `subtotal` int(10) unsigned NOT NULL,
   `discount` int(10) unsigned NOT NULL DEFAULT 0,
   `tax` int(10) unsigned NOT NULL DEFAULT 0,
-  `total` int(10) unsigned NOT NULL DEFAULT 0,
-  `paid` ENUM('room sales', 'credit card', 'cash', 'print') NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
-  `waiter` varchar(100) DEFAULT NULL,
-  `source` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`number`)
+  `status` ENUM('open', 'printed', 'paid', 'invalid') NOT NULL DEFAULT 'open',
+  `payment_mode` ENUM('room sales', 'credit card', 'cash') NOT NULL,
+  `printed_by` int(10) unsigned NOT NULL,
+  `created_on` datetime DEFAULT NULL,
+  `printed_on` datetime DEFAULT NULL,
+  `closed_on` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`waiter_id`) REFERENCES `users`(`id`)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`location_id`) REFERENCES `locations`(`id`)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`printed_by`) REFERENCES `users`(`id`)
+  ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
 --
--- Definition of table `menu`
+-- Definition of table `bill_items`
 --
 
-DROP TABLE IF EXISTS `menu`;
-CREATE TABLE `menu` (
-  `name` varchar(100) NOT NULL,
-  `cost` int(10) unsigned NOT NULL DEFAULT 0,
-  `section` ENUM('Beverages','Food','Bar') NOT NULL,
-  PRIMARY KEY (`name`)
-) ENGINE=InnoDB;
-
---
--- Definition of table `orders`
---
-
-DROP TABLE IF EXISTS `orders`;
-CREATE TABLE `orders` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `menu` varchar(100) NOT NULL,
+DROP TABLE IF EXISTS `bill_items`;
+CREATE TABLE `bill_items` (
+  `bill_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `menu_item_id` int(10) unsigned NOT NULL,
   `quantity` int(10) unsigned NOT NULL,
-  `cost` int(10) unsigned NOT NULL ,
-  `source` varchar(50) DEFAULT NULL,
-  `status` ENUM('open', 'close') NOT NULL,
-  PRIMARY KEY (`id`)
+  `added_on` datetime DEFAULT NULL,
+  `added_by` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`bill_id`, `menu_item_id`),
+  FOREIGN KEY (`bill_id`) REFERENCES `bills`(`id`)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`menu_item_id`) REFERENCES `menu_items`(`id`)
+  ON DELETE RESTRICT ON UPDATE CASCADE,
+  FOREIGN KEY (`added_by`) REFERENCES `users`(`id`)
+  ON DELETE RESTRICT ON UPDATE CASCADE
 ) ENGINE=InnoDB;
 
 --
--- Definition of table `ticket`
+-- Definition of table `db_log`
 --
 
-DROP TABLE IF EXISTS `ticket`;
-CREATE TABLE `ticket` (
-  `menu` varchar(100) DEFAULT NULL,
-  `carry` int(10) unsigned DEFAULT NULL,
-  `quantity` int(10) unsigned NOT NULL,
-  `dated` date NOT NULL,
-  `total` int(10) unsigned NOT NULL,
-  FOREIGN KEY (`menu`) REFERENCES `menu`(`name`)
-  ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB;
-
---
--- Definition of table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `username` varchar(50) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `designation` varchar(50) NOT NULL,
-  `order_manage` ENUM('no', 'yes') NOT NULL DEFAULT 'no',
-  `menu_manage` ENUM('no', 'yes') NOT NULL DEFAULT 'no',
-  `database_manage` ENUM('no', 'yes') NOT NULL DEFAULT 'no',
-  `report_manage` ENUM('no', 'yes') NOT NULL DEFAULT 'no',
-  `user_manage` ENUM('no', 'yes') NOT NULL DEFAULT 'no',
-  PRIMARY KEY(`username`)
-) ENGINE=InnoDB;
+DROP TABLE IF EXISTS `db_log`;
+CREATE TABLE `db_log` (
+  `timed_at` datetime NOT NULL,
+  `operation` ENUM('backup', 'restore') NOT NULL,
+  `user` int(10) unsigned NOT NULL
+) ENGINE=Archive;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
@@ -113,5 +200,4 @@ CREATE TABLE `user` (
 
 GRANT ALL PRIVILEGES ON `checkers`.* TO 'checkers'@'localhost' IDENTIFIED BY 'check123';
 
-INSERT INTO `user` (`username`,`password`,`name`,`designation`,`order_manage`,`menu_manage`,`database_manage`,`report_manage`,`user_manage`)
-            VALUES ('admin','admin','Administrator','Administrator','yes','yes','yes','yes','yes');
+INSERT INTO `users` VALUES (0,'admin',sha('admin'),'Admin','edit','edit','edit','edit','edit','edit', 'edit');
