fe0d846efb9d490c2d95e9b8a22c65330d2e395b
agyzone <agyzone@07daca17-3fa5-4a6f-b644-4c2ff32e7089>
2009-12-12 08:56:24 +0000

    adjusted the bill size to match silkensands bill standard
    git-svn-id: https://checkers.unfuddle.com/svn/checkers_checkers@105 07daca17-3fa5-4a6f-b644-4c2ff32e7089

--- tags/Silken Sands Release/application/views/order/bill_print.php
+++ tags/Silken Sands Release/application/views/order/bill_print.php
@@ -1,10 +1,10 @@
-<div align='center' style="width:530px; overflow:visible;">
-    <div align='center' style="width:530px;">
+<div align='left' style="width:300px; overflow:visible;">
+    <div align='center' style="width:300px;">
         <span style="font-size:28px">MANHATTAN</span>
         <br>
         <strong>Restaurant & Bar</strong>
         <br>
-        <div style="font-size:10px; width:390px; border-bottom:thin black solid;padding-top:5px;padding-left:5px;">
+        <div style="font-size:9px; width:295px; border-bottom:thin black solid;padding-top:5px;">
             Plot No. 22 & 23, Adsurlim, Dando Ground, P.O. Benaulim
             <br>
             Salcete, Goa 403716
@@ -12,8 +12,8 @@
             Tel: 0832-2789786-69-70  Email: silkensands@sancharnet.in
         </div>
     </div>
-    <div align='left' style="padding-top:25px;width:530px;">
-        <table cellpadding='3' style="width:inherit;">
+    <div align='left' style="padding-top:15px;width:300px;font-size:12px">
+        <table cellpadding='3' style="width:inherit;font-size:12px">
             <tr>
                 <td align='left'>
                     <strong>Date: </strong>
@@ -33,7 +33,7 @@
                 </td>
             </tr>
             <tr>
-                <td colspan='2' style="padding-top:10px;padding-bottom:10px;border-collapse:collapse; border-bottom: thin black solid;">
+                <td colspan='2' style="padding-top:8px;padding-bottom:12px;border-collapse:collapse; border-bottom: thin black solid;font-size:12px">
                     <strong>Name: </strong>
                     <u>
                         <?
@@ -63,16 +63,16 @@
         </table>
     </div>
     <br>
-    <div align='center' style="width:530px;">
-        <table border='1' cellpadding='2' style="border-collapse: collapse;width:inherit;">
+    <div align='center' style="width:300px;font-size:12px">
+        <table border='1' cellpadding='2' style="border-collapse: collapse;width:inherit;font-size:12px">
             <?
             $i = 0;
             if ( isset ($orders))
             {
-                echo "<tr><th width=50>Sr. No.</th>";
+                echo "<tr><th style=\"width:12%\">Sr. No.</th>";
                 echo "<th>Item name</th>";
-                echo "<th width=50>Qnty</th>";
-                echo "<th width=50>Cost</th></tr>";
+                echo "<th style=\"width:12%\">Qnty</th>";
+                echo "<th style=\"width:12%\">Cost</th></tr>";
                 foreach ($orders as $o)
                 {
                     echo "<tr><td align='center'>".++$i."</td>";
@@ -85,9 +85,9 @@
         </table>
     </div>
     <br>
-    <div style="width:530px;height:150px;">
-        <div style="width:175px; float:left;text-align:center">
-            <span style="font-size:15px;">TIN NO: 30181105939</span>
+    <div style="width:300px;height:150px;font-size:12px">
+        <div style="width:125px; float:left;text-align:center">
+            <span style="font-size:12px;">TIN NO: 30181125939</span>
             <br>
             <br>
             <br>
@@ -99,9 +99,9 @@
                 Prepared By
             </label>
         </div>
-        <div style="width:220px; float:right;">
+        <div style="width:175px; float:right;font-size:12px;">
             <?
-            echo "<table cellpadding='3' style=\"border-collapse:collapse;\">";
+            echo "<table cellpadding='3' style=\"border-collapse:collapse;font-size:12px\">";
             if ( isset ($details))
             {
                 echo "<tr><td align='right'><b>Sub Total: </b></td><td><u>Rs. ".$details->subtotal."</u><br></td></tr>";
