95a7530d94f8d459cee18a82ab13f68f4b7266a5
agyzone <agyzone@07daca17-3fa5-4a6f-b644-4c2ff32e7089>
2010-02-14 19:39:28 +0000

    inserted additional field for KOT no. changed the date format
    git-svn-id: https://checkers.unfuddle.com/svn/checkers_checkers@106 07daca17-3fa5-4a6f-b644-4c2ff32e7089

--- tags/Silken Sands Release/application/views/order/bill_print.php
+++ tags/Silken Sands Release/application/views/order/bill_print.php
@@ -1,10 +1,10 @@
-<div align='left' style="width:300px; overflow:visible;">
-    <div align='center' style="width:300px;">
+<div align='left' style="width:760px; overflow:visible;">
+    <div align='center' style="width:760px;">
         <span style="font-size:28px">MANHATTAN</span>
         <br>
         <strong>Restaurant & Bar</strong>
         <br>
-        <div style="font-size:9px; width:295px; border-bottom:thin black solid;padding-top:5px;">
+        <div style="font-size:9px; width:755px; border-bottom:2px black solid;padding-top:5px;">
             Plot No. 22 & 23, Adsurlim, Dando Ground, P.O. Benaulim
             <br>
             Salcete, Goa 403716
@@ -12,13 +12,13 @@
             Tel: 0832-2789786-69-70  Email: silkensands@sancharnet.in
         </div>
     </div>
-    <div align='left' style="padding-top:15px;width:300px;font-size:12px">
+    <div align='left' style="padding-top:15px;width:760px;font-size:12px">
         <table cellpadding='3' style="width:inherit;font-size:12px">
             <tr>
                 <td align='left'>
-                    <strong>Date: </strong>
+                    <strong>Datetime: </strong>
                     <u>
-                        <?= date('d-m-y') ?>
+                        <?= date('F j, Y, g:i a'); ?>
                     </u>
                 </td>
                 <td align='right'>
@@ -33,7 +33,7 @@
                 </td>
             </tr>
             <tr>
-                <td colspan='2' style="padding-top:8px;padding-bottom:12px;border-collapse:collapse; border-bottom: thin black solid;font-size:12px">
+                <td colspan='2' style="padding-top:8px;padding-bottom:12px;border-collapse:collapse; border-bottom: 2px black solid;font-size:12px">
                     <strong>Name: </strong>
                     <u>
                         <?
@@ -42,15 +42,21 @@
                     </u>
                 </td>
             </tr>
-            <tr>
+        	<tr>
                 <td align='left'>
+                    <strong>KOT No.:</strong>
+                    &nbsp;
+                </td>
+                <td align='right'>
                     <u>
                         <?
                         echo $details->source;
                         ?>
                     </u>
                 </td>
-                <td align='right'>
+            </tr>
+            <tr>
+                <td colspan='2' align='right'>
                     <strong>Waiter Name: </strong>
                     <u>
                         <?
@@ -63,14 +69,14 @@
         </table>
     </div>
     <br>
-    <div align='center' style="width:300px;font-size:12px">
-        <table border='1' cellpadding='2' style="border-collapse: collapse;width:inherit;font-size:12px">
+    <div align='center' style="width:760px;font-size:12px">
+        <table border='2' cellpadding='2' style="border: 1px black solid;border-collapse: collapse;width:inherit;font-size:12px">
             <?
             $i = 0;
             if ( isset ($orders))
             {
                 echo "<tr><th style=\"width:12%\">Sr. No.</th>";
-                echo "<th>Item name</th>";
+                echo "<th align='left'>Item name</th>";
                 echo "<th style=\"width:12%\">Qnty</th>";
                 echo "<th style=\"width:12%\">Cost</th></tr>";
                 foreach ($orders as $o)
@@ -85,7 +91,7 @@
         </table>
     </div>
     <br>
-    <div style="width:300px;height:150px;font-size:12px">
+    <div style="width:760px;height:150px;font-size:12px">
         <div style="width:125px; float:left;text-align:center">
             <span style="font-size:12px;">TIN NO: 30181125939</span>
             <br>
@@ -99,7 +105,7 @@
                 Prepared By
             </label>
         </div>
-        <div style="width:175px; float:right;font-size:12px;">
+        <div style="width:300px; float:right;font-size:12px;">
             <?
             echo "<table cellpadding='3' style=\"border-collapse:collapse;font-size:12px\">";
             if ( isset ($details))
@@ -108,7 +114,7 @@
                 echo "<tr><td align='right'><b>Amt. after Discount: </b></td><td><u>Rs. ".floatval($details->subtotal-$details->discount)." </u><br></td></tr>";
                 echo "<tr><td align='right'><b>Tax: </b></td><td><u>Rs. ".$details->tax." </u><br></td></tr>";
                 echo "<tr><td>&nbsp;</td></tr>";
-                echo "<tr><td align='right'><b>TOTAL: </b></td><td style=\"border-bottom: medium double black;\">Rs. ".intval($details->total)."<br></td></tr>";
+                echo "<tr><td align='right'><b>TOTAL: </b></td><td style=\"border-bottom: medium double black;font-size:14px;\">Rs. ".intval($details->total)."<br></td></tr>";
             }
             echo "</table>";
             ?>
